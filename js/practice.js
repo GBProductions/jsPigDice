7ab68e1c2fdff3076b1d73e94ca453c8a2cf798e
drewparker <drewjparker91@gmail.com>
2020-08-26 16:29:29 -0700

    mess up code Co-authored-by:Garrett Brown<garrettpaulbrown@gmail.com

---
+++ js/practice.js
@@ -0,0 +1,19 @@
+Game logic
+  total score >= 100, game over 
+  2 players
+  rotate turns
+
+Player turn
+  roll Dye 
+      if dye = 1 turn is over
+      if dye > 1 add to turnScore
+      if "roll", roll again
+      if "hold" add turnScore to totalScore, turn is over
+
+Dice logic
+  randomizer
+
+User logic
+  check for winner if total score >=100
+  Update Scoreboard
+  Winner Notification
--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,85 @@
-/*
-
-Pig Rules:
-
-Rule 1: There are two players with one cube shaped die (6 sides).
-Rule 2: Each player takes turns rolling the single die.
-Rule 3: Every player begins their turn with a roll of the die. If they roll a one (1) at anytime, their turn is immediately over, and they receive no points for that turn.
-Rule 4: If the player receives any number other than one (1) they may choose to keep that number, or roll again.  If they roll again, they add the new rolled number to their total. (Ex: If they roll 6 and then roll a 5, the total is now 11).
-Rule 4: A players turn ends when they decide to bank their number and add it to their personal total (or they roll a one(1) and receive no points).
-Rule 5: The first player to 100 points in their personal total wins.
-
-
-*/
-
-
 //BUSINESS LOGIC FOR GAME
-// PigDiceGame - object
-//     changeTurn function?
-//     playerRoll function?
-//     playerHold function?
-    
 
 function PigDiceGame() {
   this.players = [];
-  this.winningScore = 100
-  this.playerTurn = 1;
+  this.winningScore = 100;
 }
+
+//DICE RANDOMIZER
+PigDiceGame.prototype.playerRoll = function(){
+  return Math.ceil(Math.random()*6)
+}
+
+
+PigDiceGame.prototype.playerHold = function(){
+}
+  
+
+
+
+//BUSINESS LOGIC FOR PLAYERS
+
+
+
+function Player(){
+  this.currentScore = 0;
+  this.totalScore = 0;
+};
+
+PigDiceGame.prototype.takeTurn = function() {
+  let turnScore = 0 
+  let turnOver = false;
+
+  while (!turnOver) {
+    let roll = this.randomizer();
+    if (roll === 1) {
+      turnOver = true;
+      console.log("11111")
+    }else if (player.playerHold) {
+      this.totalScore += this.currentScore;
+      turnOver = true
+    }else {
+      if (player.playerRoll) {
+      this.currentScore += this.randomizer;
+      turnOver = false      
+      }
+    }
+  }
+  return this.totalScore
+}
+  
+
+PigDiceGame.prototype.rollDice = function()
+  let randomInt = Math.ceil(Math.random()*6);
+    this.rollDice = randomInt;
+
+    if(randomInt === 1) {
+      this.currentScore = 0;
+    } else { (randomInt > 1) 
+      this.totalScore = this.currentScore + randomInt;
+    }
+//USER INTERFACE LOGIC
+
+$(document).ready(function() {
+  let pigDice = new PigDiceGame();
+  let playerOne = new Player();
+  let playerTwo = new Player();
+}
+  
+
+// roll 
+// Player 1 - object
+
+// player 2 - object
+
+// Dice - object
+//     randomNumber function?
+
+// takeTurn method
+// Display current player turn
+
+// total  score display
+
 
 // PigDiceGame.prototype.changeTurn = function (){
 //   if (this.playerTurn === 1){
@@ -34,8 +89,9 @@ function PigDiceGame() {
 //   }
 // }
 
-PigDiceGame.prototype.playerRoll = function(){
-  let roll = randomizer(1, 6);
+// Player - object
+//     roll function?
+//     hold function?
 
 //   switch(this.playerTurn){
 //   case 1:
@@ -58,75 +114,11 @@ PigDiceGame.prototype.playerRoll = function(){
 //     };
 // };
 
-PigDiceGame.prototype.playerHold = function(){
 
-}
-
-function randomizer
-  return Math.ceil(Math.random()*6)
-
-
-
-//BUSINESS LOGIC FOR PLAYERS
-// Player - object
-//     roll function?
-//     hold function?
-
-
-function Player(){
-  this.currentScore = 0;
-  this.totalScore = 0;
-};
-
-this.prototype.takeTurn = function() {
-  let turnScore = 0 
-  let turnOver = false;
-
-  while (!turnOver) {
-    let roll = this.randomizer();
-    if (roll === 1) {
-      turnOver = true;
-    }else if{
-      let 
-      if (player.hold) {
-        this.totalScore += this.currentScore;
-        turnOver = true
-      }else {
-        if (player.roll) {
-        this.currentScore += this.randomizer;
-        turnOver = false
-      }
-    }
-  }
-  return this.totalScore
-}
-
-
-
-
-
-
-
-
-//USER INTERFACE LOGIC
-
-$(document).ready(function() {
-  let pigDice = new PigDiceGame();
-  let playerOne = new Player();
-  let playerTwo = new Player();
-
-}
-  
-
-// roll 
-// Player 1 - object
-
-// player 2 - object
-
-// Dice - object
-//     randomNumber function?
-
-// takeTurn method
-// Display current player turn
-
-// total  score display
+// Pig Rules:
+// Rule 1: There are two players with one cube shaped die (6 sides).
+// Rule 2: Each player takes turns rolling the single die.
+// Rule 3: Every player begins their turn with a roll of the die. If they roll a one (1) at anytime, their turn is immediately over, and they receive no points for that turn.
+// Rule 4: If the player receives any number other than one (1) they may choose to keep that number, or roll again.  If they roll again, they add the new rolled number to their total. (Ex: If they roll 6 and then roll a 5, the total is now 11).
+// Rule 4: A players turn ends when they decide to bank their number and add it to their personal total (or they roll a one(1) and receive no points).
+// Rule 5: The first player to 100 points in their personal total wins.
